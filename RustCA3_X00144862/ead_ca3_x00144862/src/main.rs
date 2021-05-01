22a3975d33ff9bd263015167db3b9444b2b0da0b
DylanNelson99 <43639115+DylanNelson99@users.noreply.github.com>
2021-05-01 23:12:24 +0100

    Update to my CA3 Rust

--- RustCA3_X00144862/ead_ca3_x00144862/src/main.rs
+++ RustCA3_X00144862/ead_ca3_x00144862/src/main.rs
@@ -1,58 +1,80 @@
 //Dylan Nelson - X00144862
 
 fn main() {
-    //3 different Longjumpers using Vector
-    let mut array_of_jumpers: Vec<LongJumper> = vec![];
+    //Update each jumper
+    let mut mp = LongJumper::new(String::from("Dylan Nelson"), EventType::Mens, 8.5);
+    mp.update_stats(8.2);
+    mp.update_stats(8.1);
+    mp.update_stats(8.6);
 
-    array_of_jumpers.push(LongJumper{
-        name: "Dylan".to_string(),
-        gendereventtype : "Male".to_string(),
-        personalbestmeters: 12.2,
-        bestseasonjump: 9.3,
-        noofcompjumps: 20
-    });
-    array_of_jumpers.push(LongJumper{
-        name: "Tara".to_string(),
-        gendereventtype : "Female".to_string(),
-        personalbestmeters: 9.3,
-        bestseasonjump: 8.1,
-        noofcompjumps: 36
-    });
-    array_of_jumpers.push(LongJumper{
-        name: "Thomas".to_string(),
-        gendereventtype : "Male".to_string(),
-        personalbestmeters: 14.1,
-        bestseasonjump: 14.1,
-        noofcompjumps: 24
-    });
+    let mut tb = LongJumper::new(String::from("Tara MacCourt"), EventType::Womens, 7.17);
+    tb.update_stats(7.0);
+    tb.update_stats(6.7);
 
-     println! {"{:?}",array_of_jumpers}
+    let mut br = LongJumper::new(String::from("Roisin Brien"), EventType::Womens, 7.0);
+    br.update_stats(7.12);
+    br.update_stats(6.9);
 
-//Filter using vector  and iterator
-     let new_arr = array_of_jumpers
-     .iter()
-     .filter(|s| s.bestseasonjump == personalbestmeters);
- for elem in new_arr {
-     println! {"{:?}", elem};
- }
+    //Add jumpers to a vector
+    let mut jumpers: Vec<LongJumper> = vec![];
+    jumpers.push(mp);
+    jumpers.push(tb);
+    jumpers.push(br);
+
+    //Display jumpers stats
+    println!("Jumpers {:?}", jumpers);
+
+    // get jumpers who have had a PB in this season
+    let seasons_best = jumpers
+        .iter()
+        .filter(|lj| lj.seasons_best == lj.personal_best) //using iterator and filter method
+        .collect::<Vec<&LongJumper>>();
+    println!("PB this season {:?}", seasons_best);
+
+    for i in 0..jumpers.len() -1 { //Display jumpers whos PB was in this season
+        jumpers[i].close_season();
+    }
 }
 
-#[derive(Debug)]                   //Struct for LongJumper
-struct LongJumper{
+#[derive(Clone, Debug)]
+enum EventType {
+    Mens,
+    Womens,
+}
+
+#[derive(Debug)]                    //Struct for a Long Jumper
+struct LongJumper {
     name: String,
-    gendereventtype: String,
-    personalbestmeters: f64,
-    bestseasonjump: f64,
-    noofcompjumps: u32
+    gender: EventType,              // Male or Female
+    personal_best: f64,             // in metres
+    seasons_best: f64,              // in metres
+    season_jumps: u32,              // no of jumps in this season
 }
 
-// ***Could not get Working was filled with errors ***
-impl LongJumper{
- fn newLongJumper(&self,name:String, gendereventtype:String, personalbestmeters: f64, bestseasonjump: f64 ,noofcompjumps:u32) -> LongJumper{
-     self.name = name;
-     self.gendereventtype = gendereventtype;
-    self.personalbestmeters = personalbestmeters;
-     self.bestseasonjump = 0;
-     self.noofcompjumps = 0;
-}
+impl LongJumper {                   //implementation of Long jumper function to allow for update
+    fn new(name: String, gender: EventType, personal_best: f64) -> LongJumper {
+        LongJumper {
+            name,
+            gender,
+            personal_best,
+            seasons_best: 0.,        // set to 0.0
+            season_jumps: 0,        // set to 0
+        }
+    }
+
+  
+    fn update_stats(&mut self, jump: f64) {   // update stats for season
+        if jump > self.personal_best {
+            self.personal_best = jump;
+            self.seasons_best = jump;
+        } else if jump > self.seasons_best {
+            self.seasons_best = jump;
+        }
+        self.season_jumps += 1;
+    }
+
+    fn close_season(&mut self) {     // close the season
+        self.seasons_best = 0.;
+        self.season_jumps = 0;
+    }
 }
