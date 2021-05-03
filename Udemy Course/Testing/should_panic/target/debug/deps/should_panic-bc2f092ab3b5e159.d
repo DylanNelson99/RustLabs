c2255db903d4270dadb5be6203fa6cfca5473a6b
DylanNelson99 <43639115+DylanNelson99@users.noreply.github.com>
2021-05-03 18:40:14 +0100

    Delete Udemy Course/Testing/should_panic directory

--- Udemy Course/Testing/should_panic/target/debug/deps/should_panic-bc2f092ab3b5e159.d
+++
@@ -1,5 +0,0 @@
-E:\Rust\Programs\Test\should_panic\target\debug\deps\libshould_panic-bc2f092ab3b5e159.rlib: src\lib.rs
-
-E:\Rust\Programs\Test\should_panic\target\debug\deps\should_panic-bc2f092ab3b5e159.d: src\lib.rs
-
-src\lib.rs:
--- Udemy Course/Testing/should_panic/target/debug/deps/should_panic-bd5abca24c8660b9.d
+++
@@ -1,5 +0,0 @@
-E:\Rust\Programs\Test\should_panic\target\debug\deps\should_panic-bd5abca24c8660b9.exe: src\lib.rs
-
-E:\Rust\Programs\Test\should_panic\target\debug\deps\should_panic-bd5abca24c8660b9.d: src\lib.rs
-
-src\lib.rs:
