69ade7c319b5901301510b5130c01c3992383224
Dylan Nelson <dylad141@gmail.com>
2021-05-04 13:27:13 +0100

    Small changes to IF/ELSE

--- Udemy Course/IF+ELSE/assignment/grade.rs
+++ Udemy Course/IF+ELSE/assignment/grade.rs
@@ -1,17 +1,23 @@
 use std::io;
 fn main() {
-	let mut per=String::new();
+	let mut percent=String::new();
 	println!("Enter your percentage");
-	io::stdin().read_line(&mut per).expect("Fail");
-	let per:i32=per.trim().parse().expect("Fail");
-	if per>=90 {
-		println!("A Grade");
-	}else if per>=80  {
-		println!("B Grade")
-	}else if per>=70{
-		println!("C Grade");
-	}else if per>=60 {
-		println!("D Grade");
+	io::stdin().read_line(&mut percent).expect("Fail");
+	let percent:i32=percent.trim().parse().expect("Fail");
+	if percent>=80 {
+		println!("A Grade (GPA: 4.0)");
+	}else if percent>=70  {
+		println!("B+ Grade(GPA: 3.50)");
+	}else if percent>=60  {
+		println!("B Grade (GPA: 3.0)");
+	}else if percent>=55  {
+		println!("B- Grade (GPA: 2.75)");
+	}else if percent>=50  {
+		println!("C+ Grade (GPA: 2.5)");
+	}else if percent>=40{
+		println!("C Grade (GPA: 2.0)");
+	}else if percent>=35 {
+		println!("D Grade (GPA: 1.50)");
 	}else {
 		println!("Fail");
 	
--- Udemy Course/IF+ELSE/assignment/vowel.rs
+++ Udemy Course/IF+ELSE/assignment/vowel.rs
@@ -5,7 +5,9 @@ fn main() {
 	io::stdin().read_line(&mut v).expect("Failed");
 	let v:char=v.trim().parse().expect("Failed");
 	
-	if v=='a' || v=='e' || v=='i' || v=='o' || v=='u' {
+	if v=='a'|| v=='e' || v=='i' || v=='o' || v=='u' 
+    || v== 'A' || v=='E' || v=='I' || v=='O' || v=='U'
+    {
 		println!("vowel");
 	}else {
 		println!("consonant");
